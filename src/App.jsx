48340fbe81fb1e450444354fd19d6b5d431103e9
ShivamH1 <shivamhonra@gmail.com>
2024-07-31 16:07:33 +0530

    React Project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen w-screen flex'>
+      <nav className='w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5'>
+        <a className='py-2 px-5 border rounded border-blue-200 text-blue-400' href='/create'>Add New Product</a>
+        <hr className='my-3 w-[80%]'></hr>
+        <h1 className='text-xl mb-3 w-[80%]'>Category Filter</h1>
+        {/* Since the parent container (<nav>) has a width of 15%, the 80% width of the <h1> & <ul> element will be calculated as 0.8 * 15% = 12%. This means the <h1> element will take up 12% of the total width of the screen. */}
+        <ul className='w-[80%]'>
+          <li className='flex items-center mb-3'>
+            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-blue-100'></span>{" "}
+            Cat
+          </li>
+          <li className='flex items-center mb-3'>
+            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-green-100'></span>{" "}
+            Cat
+          </li>
+          <li className='flex items-center mb-3'>
+            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-red-100'></span>{" "}
+            Cat
+          </li>
+        </ul>
+      </nav>
+
+      <div className='w-[85%] bg-red-100'></div>
+    </div>
+  )
+}
+
+export default App
