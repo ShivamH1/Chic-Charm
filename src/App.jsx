b24f2ee54f5755c7295fc820f73080de79f929d9
ShivamH1 <shivamhonra@gmail.com>
2024-08-01 08:43:46 +0530

    Implemented all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import Details from "./components/Details";
 
 function App() {
+  const {search, pathname} = useLocation();
+
   return (
     <div className="h-screen w-screen flex">
+
+      {(pathname != '/' || search.length > 0) && (
+        <Link className="absolute left-[18%] top-[4%] text-red-400" to={'/'}>Home</Link>
+      )}
+
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/details/:id" element={<Details />}></Route>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductContext } from "../utils/Context";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
+  const [products] = useContext(ProductContext);
+
+  let distinct_category =
+    products && products.reduce((acc, cv) => [...acc, cv.category], []);
+
+  distinct_category = [...new Set(distinct_category)];
+
+  const color = () => {
+    return `rgba(${(Math.random() * 255).toFixed()}, ${(
+      Math.random() * 255
+    ).toFixed()}, ${(Math.random() * 255).toFixed()}, 0.4)`;
+  };
+
   return (
     <nav className="w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5">
       <a
@@ -12,20 +27,18 @@ export default function Nav() {
       <hr className="my-3 w-[80%]"></hr>
       <h1 className="text-xl mb-3 w-[80%]">Category Filter</h1>
       {/* Since the parent container (<nav>) has a width of 15%, the 80% width of the <h1> & <ul> element will be calculated as 0.8 * 15% = 12%. This means the <h1> element will take up 12% of the total width of the screen. */}
-      <ul className="w-[80%]">
-        <li className="flex items-center mb-3">
-          <span className="rounded-full mr-2 w-[15px] h-[15px] bg-blue-100"></span>{" "}
-          Cat
-        </li>
-        <li className="flex items-center mb-3">
-          <span className="rounded-full mr-2 w-[15px] h-[15px] bg-green-100"></span>{" "}
-          Cat
-        </li>
-        <li className="flex items-center mb-3">
-          <span className="rounded-full mr-2 w-[15px] h-[15px] bg-red-100"></span>{" "}
-          Cat
-        </li>
-      </ul>
+      <div className="w-[80%]">
+        {distinct_category.map((c, i) => (
+          <Link
+            key={i}
+            to={`/?category=${c}`}
+            className="flex items-center mb-3"
+          >
+            <span style={{ backgroundColor: color() }} className="rounded-full mr-2 w-[15px] h-[15px]"></span>{" "}
+            {c}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 }
